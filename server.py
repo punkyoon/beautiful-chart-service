3258b58a225a5211c24c93bee81f420bdc7a3179
punkyoon <cryptography@konkuk.ac.kr>
2017-05-23 04:17:55 +0000

    bullet chart

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, render_template
+from subprocess import call
+from flask_socketio import SocketIO, send, emit
+
+app = Flase(__name__)
+app.secret_key = 'mysecret'
+
+socket_io = SocketIO(app)
+
+# _mode = 1 or 2 or 3
+_mode = 1
+
+@app.route('/')
+def draw():
+    return render_template('main.html')
+
+# Chaninging Mode
+@socket_io.on('change mode')
+def changer(data):
+    global _mode
+    
+    print('receive data: ' + str(data))
+    # emit('draw', data, broadcast=True)
+
+if __name__ == '__main__':
+    socket_io.run(app, debug=True, host='0.0.0.0', port=80)
+    #socket_io.run(app, debug=True, host='localhost', port=8000)
