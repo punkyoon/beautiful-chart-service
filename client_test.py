fb68111bdbdad910b32c070cfe57bc6e974249d9
punkyoon <cryptography@konkuk.ac.kr>
2017-05-23 05:38:46 +0000

    stylesheet loading

---
+++ client_test.py
@@ -0,0 +1,13 @@
+import time
+from random import randrange
+from socketIO_client import SocketIO, LoggingNamespace
+
+while True:
+    #with SocketIO('localhost', 8000, LogginNamespace) as SocketIO:
+    with SocketIO('52.231.53.33', 80, LoggingNamespace) as SocketIO:
+        send_data = {
+            'test_value': randrange(50)
+        }
+
+        socketIO.emit('my event', send_data)
+        time.sleep(0.5)
