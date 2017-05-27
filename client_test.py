490a8fff041f71c6ab11518a8a3736008c81ece7
punkyoon <cryptography@konkuk.ac.kr>
2017-05-27 20:06:11 +0900

    i don't like d3.js.

--- client_test.py
+++ client_test.py
@@ -3,8 +3,8 @@ from random import randrange
 from socketIO_client import SocketIO, LoggingNamespace
 
 while True:
-    #with SocketIO('localhost', 8000, LogginNamespace) as SocketIO:
-    with SocketIO('52.231.53.33', 80, LoggingNamespace) as SocketIO:
+    with SocketIO('localhost', 8000, LogginNamespace) as SocketIO:
+    #with SocketIO('52.231.53.33', 80, LoggingNamespace) as SocketIO:
         send_data = {
             'test_value': randrange(50)
         }
