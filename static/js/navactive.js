d52361ff4171272561e08812c9e3ee07a00e6a97
punkyoon <cryptography@konkuk.ac.kr>
2017-06-19 00:50:21 +0900

    added button active toggle

---
+++ static/js/navactive.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+   $("li").click(function() {
+      // remove classes from all
+      $("li").removeClass("active");
+      // add class to the one we clicked
+      $(this).addClass("active");
+   });
+});
