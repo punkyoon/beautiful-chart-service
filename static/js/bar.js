0eb6b0cb2c046b69ec5946c7c7230a5b6cbcbdf4
punkyoon <cryptography@konkuk.ac.kr>
2017-06-19 00:07:21 +0900

    added chart examples

---
+++ static/js/bar.js
@@ -0,0 +1,39 @@
+var chart = bb.generate({
+  "data": {
+    "columns": [
+      [
+        "data1",
+        30,
+        200,
+        100,
+        400,
+        150,
+        250
+      ],
+      [
+        "data2",
+        130,
+        100,
+        140,
+        200,
+        150,
+        50
+      ]
+    ],
+    "type": "bar"
+  },
+  "bar": {
+    "width": {
+      "ratio": 0.5
+    }
+  },
+  "bindto": "#BarChart"
+});
+
+setTimeout(function () {
+	chart.load({
+		columns: [
+			['data3', 130, -150, 200, 300, -200, 100]
+		]
+	});
+}, 1000);
